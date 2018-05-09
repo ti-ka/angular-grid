e3ec15e778d7417e54618aa4cd59feef2fa2f0d9
PAHADIx <tika.pahadi@selu.edu>
2018-05-09 02:22:03 -0500

    Initial Release

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { NgGridComponent } from './ng-grid/ng-grid.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NgGridComponent,
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+    ],
+    providers: [
+        HttpClient
+    ],
+    exports: [NgGridComponent],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
